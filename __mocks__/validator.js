eb4470e8197564b52713a60eaa52e4d4571ce046
Gustavo Alves Silva <gustavo_alves777@outlook.com>
2023-05-10 17:19:29 -0300

    ensure email validator returns false

---
+++ __mocks__/validator.js
@@ -0,0 +1,5 @@
+// export const validator = {
+//   isEmail(email) {
+//     return this.isEmailValid;
+//   },
+// };
--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,14 +1,22 @@
-
 class EmailValidator {
-    isValid(email) {
-      return true
-    }
+  isValid(email) {
+    return email;
+  }
+}
+
+const makeSut = () => {
+  return new EmailValidator()
 }
 
 describe("Email validator", () => {
   test("should return true if validator return true", () => {
-    const sut = new EmailValidator();
-    const isEmailValid = sut.isValid("valid_@email.com");
+    const sut = makeSut()
+    const isEmailValid = sut.isValid(true);
     expect(isEmailValid).toBe(true);
+  });
+  test("should return true if validator return true", () => {
+    const sut = makeSut();
+    const isEmailValid = sut.isValid(false);
+    expect(isEmailValid).toBe(false);
   });
 });

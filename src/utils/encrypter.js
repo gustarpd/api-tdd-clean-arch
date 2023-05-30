4467f5e8ca15306e149a55a211519d06437223be
Gustavo Alves <gustavo_alves777@outlook.com>
2023-05-30 12:00:38 -0300

    move encrypter throws if no params are provided

--- src/utils/encrypter.js
+++ src/utils/encrypter.js
@@ -1,7 +1,11 @@
 import bcrypt from "bcrypt";
+import { MissingParamError } from "./errors/missing-params-error";
 
 export class Encrypter {
   async compare(value, hashed_value) {
+    if (!value || !hashed_value) {
+      throw new MissingParamError("value");
+    }
     const isValid = bcrypt.compare(value, hashed_value);
     return isValid;
   }
--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { Encrypter } from "./encrypter";
+import { MissingParamError } from "./errors/missing-params-error";
 
 const makeSut = () => {
   return new Encrypter();
@@ -24,5 +25,11 @@ describe("Encrypter", () => {
     await sut.compare("any_value", "hashed_value");
     expect(bcrypt.value).toBe("any_value");
     expect(bcrypt.hashed_value).toBe("hashed_value");
+  });
+
+  test("Should throw if no params are provided", async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('value'))
   });
 });

896462bfb33a56621d924fffe7ae7cb9fc4455a4
Gustavo Alves <gustavo_alves777@outlook.com>
2023-05-30 13:43:53 -0300

    move sut creation to a factory helper method

--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -6,15 +6,19 @@ class TokenGenerator {
   }
 }
 
+const makeSut = () => {
+  return new TokenGenerator();
+};
+
 describe("Token Generator", () => {
   test("Should return null if JWT returns null", async () => {
-    const sut = new TokenGenerator();
-    JWT.token = null
+    const sut = makeSut();
+    JWT.token = null;
     const token = await sut.generate("any_id");
     expect(token).toBe(null);
   });
   test("Should token f JWT retun token", async () => {
-    const sut = new TokenGenerator();
+    const sut = makeSut();
     const token = await sut.generate("any_id");
     expect(token).toBe(JWT.token);
   });

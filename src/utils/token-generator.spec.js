314b71def03ffa2ffc4e8dbd8010a609611b22df
Gustavo Alves <gustavo_alves777@outlook.com>
2023-05-30 17:31:45 -0300

    ensure LoadUserByEmailRepository return null if no user is found

--- src/utils/token-generator.spec.js
+++ src/utils/token-generator.spec.js
@@ -1,17 +1,6 @@
 import JWT from "jsonwebtoken";
 import { MissingParamError } from "./errors/missing-params-error";
 
-class TokenGenerator {
-  constructor(secret) {
-    this.secret = secret;
-  }
-  async generate(id) {
-    if(!this.secret) throw new MissingParamError('secret')
-    if(!id)  throw new MissingParamError('id')
-    return JWT.sign(id, this.secret);
-  }
-}
-
 const makeSut = () => {
   return new TokenGenerator("secret");
 };

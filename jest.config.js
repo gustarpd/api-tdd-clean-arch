b2556077d54209a562e5aa295997ff6d9ce27f67
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-05 14:28:32 -0300

    ignore main module in coverage

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ export const testEnvironment = 'node';
 export const testMatch = [
   '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
   '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  '<rootDir>/src/main/**/?(*.)+(test)'
 ];
 export const transform = {
   '^.+\\.js$': 'babel-jest',
--- src/main/middlewares/__test__/content-type.test.js
+++ src/main/middlewares/__test__/content-type.test.js
@@ -4,12 +4,11 @@ import app from "../../config/app";
 describe("Content-Type Middleware", () => {
   test("should return json content type as default", async () => {
     app.get("/text_content_type", (req, res) => {
-      res.type('xml')
+      res.type("xml");
       res.send("");
     });
 
-    request(app).get("/text_content_type")
-    .expect('content-type', /xml/)
+    request(app).get("/text_content_type").expect("content-type", /json/);
   });
 
   test("should return xml content type as default", async () => {
@@ -17,7 +16,6 @@ describe("Content-Type Middleware", () => {
       res.send("");
     });
 
-    request(app).get("/text_content_type")
-    .expect('content-type', /json/)
+    request(app).get("/text_content_type").expect("content-type", /xml/);
   });
 });

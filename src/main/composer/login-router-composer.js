a9aa9308802053a20784effd7e7a0fe3d04a257d
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-05 17:56:02 -0300

    move LoginRouter composition to own file

---
+++ src/main/composer/login-router-composer.js
@@ -0,0 +1,24 @@
+import { LoginRouter } from "../../presentation/routers/login-router.js";
+import { AuthUseCase } from "../../domain/usecases/auth-usecase.js";
+import { EmailValidator } from "../../utils/email-validator.js";
+import { LoadUserByEmailRepository } from "../../infra/repositories/load-user-by-email-repository.js";
+import { UpdateAccessTokenRepository } from "../../infra/repositories/update-access-token-repository.js";
+import { Encrypter } from "../../utils/encrypter.js";
+import { TokenGenerator } from "../../utils/token-generator.js";
+import env from '../config/env.js'
+
+
+const loadUserByEmailRepository = new LoadUserByEmailRepository();
+const updateAccessTokenRepository = new UpdateAccessTokenRepository();
+const encripter = new Encrypter();
+const tokenGenerator = new TokenGenerator(env.tokenSecret);
+const authUseCase = new AuthUseCase(
+  loadUserByEmailRepository,
+  encripter,
+  tokenGenerator,
+  updateAccessTokenRepository
+);
+const emailValidator = new EmailValidator();
+const loginRouter = new LoginRouter({ authUseCase, emailValidator });
+
+export default loginRouter

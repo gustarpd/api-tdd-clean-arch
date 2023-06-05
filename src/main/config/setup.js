e9461a0a44bef8e847960101fc19298f44ce4b54
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-05 12:20:20 -0300

    ensure request parse body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,9 @@
 import cors from "../middlewares/cors";
+import express from 'express'
+import jsonParser from "../middlewares/json-parser";
 
 export default (app) => {
   app.disable("x-powered-by");
   app.use(cors)
+  app.use(jsonParser)
 };
---
+++ src/main/middlewares/json-parser.test.js
@@ -0,0 +1,16 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("CORS middleware", () => {
+  test("should parse body as json", async () => {
+    app.get("/test_json_parser", (req, res) => {
+      res.json(req.body);
+    });
+
+    
+   request(app)
+    .post('/test_json_parser')
+    .send({ name: 'any_name' })
+    .expect({ name: 'any_name' })
+  });
+});

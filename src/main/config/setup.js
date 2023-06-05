7afb628e9e6bc17a21e7e83dc61cefa6f73d9794
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-05 12:05:55 -0300

    move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,6 @@
+import cors from "../middlewares/cors";
+
 export default (app) => {
-   app.disable("x-powered-by")
-   app.use((req, res, next) => {
-      res.set('access-control-allow-origin', '*')
-      res.set('access-control-allow-methods', '*')
-      res.set('access-control-allow-headers', '*')
-      next()
-   })
-}
+  app.disable("x-powered-by");
+  app.use(cors)
+};

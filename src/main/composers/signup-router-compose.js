9c15821eb406c633efc814395bc5d91b37e27356
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-11 13:24:18 -0300

    add tests signup controller

--- src/main/composers/signup-router-compose.js
+++ src/main/composers/signup-router-compose.js
@@ -8,12 +8,15 @@ import env from "../config/env.js";
 
 export class SignupRouterCompose {
   static compose() {
-    const addAccountRepository = new AddAccountRepository()
-    const encripter = new Encrypter()
-    const tokenGenerator = new TokenGenerator(env.tokenSecret)
-    const addAccount = new AddAccount(addAccountRepository, encripter, tokenGenerator)
-    const emailValidator = new EmailValidator()
+    const addAccountRepository = new AddAccountRepository();
+    const encripter = new Encrypter();
+    const tokenGenerator = new TokenGenerator(env.tokenSecret);
+    const addAccount = new AddAccount(
+      addAccountRepository,
+      encripter,
+      tokenGenerator
+    );
+    const emailValidator = new EmailValidator();
     return new SignUpController({ addAccount, emailValidator });
-   
   }
 }
--- src/presentation/controllers/__tests__/signup-router.spec.js
+++ src/presentation/controllers/__tests__/signup-router.spec.js
@@ -1,14 +1,15 @@
 import { SignUpController } from "../signup-controller";
+import { HttpResponse } from "../../helpers/httpReponse";
 
 class AddAccount {
   async add(name, email, password) {
-    return 'accessToken'
+    return "accessToken";
   }
 }
 
 class EmailValidator {
   async isValid(email) {
-    return true
+    return false;
   }
 }
 
@@ -18,24 +19,74 @@ const makeSut = () => {
   const signUpController = new SignUpController({ addAccount, emailValidator });
 
   return {
-   addAccount,
-   emailValidator,
-   signUpController
-  }
+    addAccount,
+    emailValidator,
+    signUpController,
+  };
 };
 
 describe("Signup Router", () => {
   const body = {
     name: "any_mail",
     email: "mail@mail.com",
-    password: "any_password"
-  }
+    password: "any_password",
+  };
+
   test("should return accessToken when calls add method", async () => {
-    const { signUpController, addAccount } = makeSut()
-    addAccount.accessToken = 'valid_token'
-    const result = await signUpController.route({ body })
-    expect(result).toHaveProperty("body")
-    expect(result.body).toHaveProperty("accessToken")
-    expect(result.statusCode).toBe(200)
+    const { signUpController, addAccount } = makeSut();
+    addAccount.accessToken = "valid_token";
+    const result = await signUpController.route({ body });
+    expect(result).toHaveProperty("body");
+    expect(result.body).toHaveProperty("accessToken");
+    expect(result.statusCode).toBe(200);
+  });
+
+  test("should throw InternalError if route method has no any httpBody", async () => {
+    const { signUpController } = makeSut();
+    const result = await signUpController.route({});
+    expect(result).toHaveProperty("body");
+    expect(result.statusCode).toBe(500);
+  });
+
+  test("should throw MissingParamError if param name are no provided", async () => {
+    const { signUpController, addAccount } = makeSut();
+    addAccount.accessToken = "valid_token";
+    const result = await signUpController.route({
+      body: {
+        name: "any_mail",
+        password: "any_password",
+      },
+    });
+    expect(result.body).toHaveProperty("error");
+    expect(result.body.error).toBe("Missing param: email");
+    expect(result.statusCode).toBe(400);
+  });
+
+  test("should throw MissingParamError if param email are no provided", async () => {
+    const { signUpController, addAccount } = makeSut();
+    addAccount.accessToken = "valid_token";
+    const result = await signUpController.route({
+      body: {
+        email: "mail@mail.com",
+        password: "any_password",
+      },
+    });
+    expect(result.body).toHaveProperty("error");
+    expect(result.body.error).toBe("Missing param: name");
+    expect(result.statusCode).toBe(400);
+  });
+
+  test("should throw MissingParamError if param password are no provided", async () => {
+    const { signUpController, addAccount } = makeSut();
+    addAccount.accessToken = "valid_token";
+    const result = await signUpController.route({
+      body: {
+        name: "any_mail",
+        email: "mail@mail.com",
+      },
+    });
+    expect(result.body).toHaveProperty("error");
+    expect(result.body.error).toBe("Missing param: password");
+    expect(result.statusCode).toBe(400);
   });
 });

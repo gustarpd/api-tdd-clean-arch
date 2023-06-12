ca8e89f5d06d425af925bbe4ab99bb7614920338
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-12 16:50:01 -0300

    rename method handle at controllers

--- src/main/routes/workspace-routes.js
+++ src/main/routes/workspace-routes.js
@@ -1,6 +1,7 @@
 import { AddWorkSpaceCompose } from '../composers/add-workspace-compose.js';
 import { ExpressRouterAdapter } from '../adapter/express-router-adapter.js';
+import { auth } from '../config/auth.js';
 
 export default (router) => {
-  router.post("/tasks", ExpressRouterAdapter.adapt(AddWorkSpaceCompose.compose()));
+  router.post("/tasks", auth,ExpressRouterAdapter.adapt(AddWorkSpaceCompose.compose()));
 };

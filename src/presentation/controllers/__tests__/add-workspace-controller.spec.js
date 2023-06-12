52098a7ba5f84d165245b861406a2873c0198a40
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-12 14:10:58 -0300

    move add-workspace to its own file

--- src/presentation/controllers/__tests__/add-workspace-controller.spec.js
+++ src/presentation/controllers/__tests__/add-workspace-controller.spec.js
@@ -20,29 +20,6 @@ const makeAddWorkSpace = () => {
   return addWorkSpace;
 };
 
-export class AddWorkSpaceController {
-  constructor(addWorkSpace) {
-    this.addWorkSpace = addWorkSpace;
-  }
-  async handle(httpRequest) {
-    try {
-      if (!httpRequest || !httpRequest.body) {
-        return HttpResponse.InternalError();
-      }
-      const { description, owner, priority, accessToken } = httpRequest.body;
-      const workspace = await this.addWorkSpace.add({
-        description,
-        owner,
-        priority,
-        accessToken,
-      });
-      return HttpResponse.ok({ workspace });
-    } catch (error) {
-      return HttpResponse.unauthorizeError();
-    }
-  }
-}
-
 const makeSut = () => {
   const addRepository = makeAddWorkSpace();
   const sut = new AddWorkSpaceController(addRepository);
---
+++ src/presentation/controllers/add-workspace-controller.js
@@ -0,0 +1,24 @@
+import { HttpResponse } from "../helpers/httpReponse";
+
+export class AddWorkSpaceController {
+  constructor(addWorkSpace) {
+    this.addWorkSpace = addWorkSpace;
+  }
+  async handle(httpRequest) {
+    try {
+      if (!httpRequest || !httpRequest.body) {
+        return HttpResponse.InternalError();
+      }
+      const { description, owner, priority, accessToken } = httpRequest.body;
+      const workspace = await this.addWorkSpace.add({
+        description,
+        owner,
+        priority,
+        accessToken,
+      });
+      return HttpResponse.ok({ workspace });
+    } catch (error) {
+      return HttpResponse.unauthorizeError();
+    }
+  }
+}

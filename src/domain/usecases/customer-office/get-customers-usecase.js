a965a0d72ea0e95c64c057985261f09d7bb41ad7
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-23 14:59:10 -0300

    Move GetAllCustomer to its own file

---
+++ src/domain/usecases/customer-office/get-customers-usecase.js
@@ -0,0 +1,19 @@
+import { CustomerOffice } from '../../entities/clients-office'
+
+class GetAllCustomer {
+  constructor(GetAllCustomerRepository) {
+    this.GetAllCustomerRepository = GetAllCustomerRepository;
+  }
+  async execute() {
+    try {
+      const update = await this.GetAllCustomerRepository.getManyById();
+      if (!update) {
+        return { message: "Nenhum cliente foi encontrado." };
+      }
+      return CustomerOffice.toData(update);
+    } catch (error) {
+      console.error(`Error: ${error}`);
+      throw new Error("Error: Failed to fetch customers.");
+    }
+  }
+}

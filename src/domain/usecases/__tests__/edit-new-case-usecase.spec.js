9324f18a7b3c260651f0ba22e5fa85565614d52f
Gustavo Alves <gustavo_alves777@outlook.com>
2023-06-29 14:00:05 -0300

    move EditCaseUseCase to its own file

--- src/domain/usecases/__tests__/edit-new-case-usecase.spec.js
+++ src/domain/usecases/__tests__/edit-new-case-usecase.spec.js
@@ -1,3 +1,5 @@
+import { EditCaseUseCase } from "../managecases/edit-case-usecase";
+
 const makeEditCaseUseCase = () => {
   class EditCaseUseCase {
     async editById(data) {
@@ -40,53 +42,6 @@ const makeEditCaseUseCase = () => {
 
   return editCaseRepository;
 };
-
-class EditCaseUseCase {
-  constructor(editCaseRepository) {
-    this.editCaseRepository = editCaseRepository;
-  }
-
-  async edit({
-    id,
-    title,
-    customer,
-    involved_parties,
-    awarded_amount,
-    status,
-    owner,
-    protocol,
-    casedata,
-    event,
-  }) {
-    if (
-      !title ||
-      !customer ||
-      !awarded_amount ||
-      !involved_parties ||
-      !status ||
-      !owner ||
-      !protocol ||
-      !casedata ||
-      !event
-    ) {
-      throw new Error("Dados de entrada incompletos");
-    }
-    const edited = await this.editCaseRepository.editById({
-      id,
-      title,
-      customer,
-      involved_parties,
-      event,
-      protocol,
-      casedata,
-      awarded_amount,
-      status,
-      owner,
-    });
-
-    return edited;
-  }
-}
 
 const makeSut = () => {
   const editCaseRepository = makeEditCaseUseCase();

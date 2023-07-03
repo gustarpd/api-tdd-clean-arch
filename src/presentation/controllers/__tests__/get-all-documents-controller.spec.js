7edc668816490a5e0bc3d1883798709bf1df3229
Gustavo Alves <gustavo_alves777@outlook.com>
2023-07-03 16:05:20 -0300

    Add GetAllDocumentsController tests

---
+++ src/presentation/controllers/__tests__/get-all-documents-controller.spec.js
@@ -0,0 +1,103 @@
+import { HttpResponse } from "../../helpers/httpReponse";
+
+class GetAllDocumentsController {
+  constructor(getAllDocumentsUseCase) {
+    this.getAllDocumentsUseCase = getAllDocumentsUseCase;
+  }
+
+  async handle(httpRequest) {
+    try {
+      if (!httpRequest) {
+        return HttpResponse.InternalError();
+      }
+
+      const documents = await this.getAllDocumentsUseCase.execute();
+      return HttpResponse.ok(documents);
+    } catch (error) {
+      console.error(error);
+      throw new Error("some error in database", error);
+    }
+  }
+}
+
+const makeGetAllDocumentRepository = () => {
+  class GetAllDocumentsRepository {
+    async getAll() {
+      return this.data;
+    }
+  }
+
+  const getAllDocument = new GetAllDocumentsRepository();
+  return getAllDocument;
+};
+
+class GetAllDocumentsUseCase {
+  constructor(getAllDocumentsRepository) {
+    this.getAllDocumentsRepository = getAllDocumentsRepository;
+  }
+
+  async execute() {
+    try {
+      const document = await this.getAllDocumentsRepository.getAll();
+      if (document.length === 0) {
+        return { message: "No documents found" };
+      }
+      return document;
+    } catch (error) {
+      throw new Error("some error in database", error);
+    }
+  }
+}
+const makeSut = () => {
+  const GetAllDocumentsRepository = makeGetAllDocumentRepository();
+  const getAllDocumentsUseCase = new GetAllDocumentsUseCase(
+    GetAllDocumentsRepository
+  );
+  const sut = new GetAllDocumentsController(getAllDocumentsUseCase);
+
+  return {
+    sut,
+    getAllDocumentsUseCase,
+    GetAllDocumentsRepository,
+  };
+};
+
+describe("GetAllDocuments controller", () => {
+  test("should return HttpRequest if request succeeds", async () => {
+    const { sut, GetAllDocumentsRepository } = makeSut();
+    GetAllDocumentsRepository.data = [
+      {
+        owner: "John Doe",
+        url: "https://www.youtube.com/watch?v=qa-4_A2uIOU",
+        title: "Fake Document 1",
+        id: "1",
+      },
+      {
+        owner: "John Doe",
+        url: "https://www.youtube.com/watch?v=qa-4_A2uIOU",
+        title: "Fake Document 2",
+        id: "2",
+      },
+    ];
+    const httpRequest = await sut.handle({});
+    expect(httpRequest.body).toEqual(GetAllDocumentsRepository.data);
+    expect(httpRequest.statusCode).toEqual(200);
+  });
+
+  test("should return 400 if no documents are not found", async () => {
+    const { sut, GetAllDocumentsRepository } = makeSut();
+    GetAllDocumentsRepository.data = [];
+    const httpRequest = await sut.handle({});
+    expect(httpRequest.body).toEqual({ message: "No documents found" });
+  });
+
+  test("should return 400 if no documents are not found", async () => {
+    const repository = {
+      execute: () => {
+        throw new Error("No documents found");
+      },
+    };
+    const sut = new GetAllDocumentsController(repository);
+    expect(sut.handle({})).rejects.toThrow(new Error("some error in database"));
+  });
+});
